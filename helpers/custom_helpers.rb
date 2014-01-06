440b9ff793bb4693f536c5f33d33eac05f06e376
Cloud Ride <cloudrideday1@gmail.com>
2014-01-06 20:30:35 +0100

    Portfolio: tags, details pages, data

--- helpers/custom_helpers.rb
+++ helpers/custom_helpers.rb
@@ -1,3 +1,11 @@
-def main_menu_item(name, link, description)
+def main_menu_item name, link, description
   link_to "#{name}<span><span class='hidden-desktop'>-</span>#{description}</span>", link
+end
+
+def portfolio_tags
+  tags = []
+  data.portfolio.list.each do |f|
+    f.tags.each {|ff| tags << ff }
+  end
+  tags.uniq!
 end
